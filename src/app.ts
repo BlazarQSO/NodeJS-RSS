d350726079438c9684d45fd91ec85c38add4e06a
BlazarQSO <BlazarQSO@gmail.com>
2021-06-06 18:00:30 +0300

    feat: add logger and loggingMiddleware

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,18 @@ import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 import path from 'path';
 import YAML from 'yamljs';
+
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
+import * as Middleware from './middleware';
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
+
+app.use(Middleware.logging);
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
