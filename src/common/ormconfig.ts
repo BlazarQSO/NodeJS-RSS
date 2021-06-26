de02895c6904eb40ef3b01024cb6d0e76cb25a95
Siarhei Iukou <Siarhei_Iukou@epam.com>
2021-06-26 07:43:46 +0300

    feat: add migration

--- src/common/ormconfig.ts
+++ src/common/ormconfig.ts
@@ -1,17 +1,20 @@
 import { ConnectionOptions } from 'typeorm';
+import { User, Task, Board } from '../entities';
 
 export const ormConfig: ConnectionOptions = {
-  type: 'postgres',  
-  synchronize: false,
+  type: 'postgres',    
   host: process.env['POSTGRES_HOST'],
   port: Number(process.env['POSTGRES_PORT']),
   database: process.env['POSTGRES_DB'],
   username: process.env['POSTGRES_USER'],
   password: process.env['POSTGRES_PASSWORD'],
-  entities: ['src/entities/**/*.ts'],
-//   entities: [`${__dirname}/resources/**/*.entity{.ts,.js}`],  
-//   migrations: ['src/migrations/*.ts'],
-//   cli: {
-//     migrationsDir: 'src/migrations',
-//   },
+  logging: false,
+  migrationsRun: false,
+  synchronize: false,
+  dropSchema: false,
+  entities: [User, Board, Task],  
+  migrations: ['./src/migrations/**/*.ts'],  
+  cli: {
+    migrationsDir: 'src/migrations',
+  },  
 };
---
+++ src/entities/index.ts
@@ -0,0 +1,9 @@
+import { User } from './user.entity';
+import { Board } from './board.entity';
+import { Task } from './task.entity';
+
+export {
+  User,
+  Board,  
+  Task,
+};
